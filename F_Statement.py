029129c220d2da0fca1462d4fe06c7d18d8dc528
Nykim <naroh1993@gmail.com>
2019-03-19 10:33:03 +0900

    first commit 남엽

--- F_Statement.py
+++ F_Statement.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import sys
 import io
 
+#남엽 work
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
 
